ab0611ca9525d348bcd38abbda6a50419824ea4d
GiseDev <glazcano.dev@gmail.com>
2022-02-03 00:13:51 -0300

    inclusion de parametro stage

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -1,32 +1,30 @@
 def call(){
-    pipeline {
-        agent any
-        environment {
-            NEXUS_USER         = credentials('NEXUS-USER')
-            NEXUS_PASSWORD     = credentials('NEXUS-PASS')
-        }
-        parameters {
-            choice(
-                name:'compileTool',
-                choices: ['Maven', 'Gradle'],
-                description: 'Seleccione herramienta de compilacion'
-            )
+  pipeline {
+      agent any
+      environment {
+          NEXUS_USER         = credentials('NEXUS-USER')
+          NEXUS_PASSWORD     = credentials('NEXUS-PASS')
+      }
+      parameters {
+            choice choices: ['maven', 'gradle'], description: 'Seleccione una herramienta para preceder a compilar', name: 'compileTool'
             text description: 'Enviar los stages separados por ";"... Vacío si necesita todos los stages', name: 'stages'
-        }
-        stages {
-            stage("Pipeline"){
-                steps {
-                    script{
-                        sh "env"
-                        env.TAREA = ""
-                        if(params.compileTool == 'maven'){
-                            maven.call(params.stages);
-                        }else{
-                            gradle.call(params.stages)
-                        }
-                    }
-                }
-                post{
+      }
+      stages {
+          stage("Pipeline"){
+              steps {
+                  script{
+                      sh "env"
+                      env.TAREA = ""
+                      if(params.compileTool == 'maven'){
+
+                        maven.call(params.stages);
+                      }else{
+                        echo 'prellamada'
+                        gradle.call(params.stages)
+                      }
+                  }
+              }
+              post{
                     success{
                         slackSend color: 'good', message: "Giselle [${JOB_NAME}] [${BUILD_TAG}] Ejecucion Exitosa. Ejecutado con [${params.compileTool}]", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
                     }
@@ -34,8 +32,8 @@ def call(){
                         slackSend color: 'danger', message: "Giselle [${env.JOB_NAME}] [${BUILD_TAG}] Ejecucion fallida en stage [${env.TAREA}]. Ejecutado con [${params.compileTool}]", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
                     }
                 }
-            }
-        }
-    }
+          }
+      }
+  }
 }
 return this;
