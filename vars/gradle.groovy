0dbfadc97416025e14e990dc2f73fc1ef4d1f76b
GiseDev <glazcano.dev@gmail.com>
2022-02-03 01:12:12 -0300

    inclusion de parametro stage

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -2,8 +2,6 @@ def call(stages)
 {
     echo 'Stages a ejecutar :' + stages
     def mapa = [100 : 'Continue', 200 : 'OK', 400 : 'badRequest']
-
     echo 'valor => ' + mapa.size()
-​
 }
 return this;
